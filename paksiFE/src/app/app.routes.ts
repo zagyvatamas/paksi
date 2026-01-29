8be47e9f488816b4fb014d1b03fce56d604f4e2c
zagyvatamas <xnoszix@gmail.com>
2026-01-29 21:26:40 +0100

    Landing page navbar with design

--- paksiFE/src/app/app.routes.ts
+++ paksiFE/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
 import { ProfileComponent } from '../components/profile/profile.component';
+import { LandingComponent } from '../components/landing/landing.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: 'profile', component: ProfileComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
+    { path: 'landing', component: LandingComponent},
+    { path: '', redirectTo: 'landing', pathMatch: 'full' }
 ];

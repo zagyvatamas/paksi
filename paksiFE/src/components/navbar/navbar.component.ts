9feed524280a0eef76e4a3fc9502f74ce8c69b3f
zagyvatamas <xnoszix@gmail.com>
2026-02-25 16:56:14 +0100

    navbar responsive design

--- paksiFE/src/components/navbar/navbar.component.ts
+++ paksiFE/src/components/navbar/navbar.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  isMenuCollapsed = true;
 }
